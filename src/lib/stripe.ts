abf70695bdf2995795ad08228594075aad93a615
Meghal Parikh <meghalparikh@Meghals-Mac-mini-2.local>
2026-02-23 16:30:10 -0600

    vercel fix

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,7 @@
 
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_fallback', {
     apiVersion: '2026-01-28.clover',
     typescript: true,
 });
